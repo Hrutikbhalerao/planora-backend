e7ec0238ff92bba5687bb6845576c4ed6cae676e
Hrutikbhalerao <rpbbhalerao2000@gmail.com>
2025-05-30 15:45:39 +0530

    Add missing dependencies

--- tour_travel_backend/urls.py
+++ tour_travel_backend/urls.py
@@ -1,7 +1,12 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.http import JsonResponse
+
+def home(request):
+    return JsonResponse({"message": "Planora backend is up and running!"})
 
 urlpatterns = [
+    path('', home),  # ← add this to handle root URL
     path('admin/', admin.site.urls),
-    path('api/', include('api.urls')),  # ✅ just 'api/' here
+    path('api/', include('api.urls')),
 ]
